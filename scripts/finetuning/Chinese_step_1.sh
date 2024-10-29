1711b0ae4b9590bba1c502a0a06e5b3b50da86b7
Raphael Baena <38126139+raphael-baena@users.noreply.github.com>
2024-10-29 21:00:56 +0100

    Update Chinese_step_1.sh

--- scripts/finetuning/Chinese_step_1.sh
+++ scripts/finetuning/Chinese_step_1.sh
@@ -2,7 +2,6 @@ dataset_path="/home/rbaena/datasets"
 old_charset_path="$dataset_path/HWDB_v1/charset.pkl"
 
 python finetuning.py \
-	--CTC_training \
 	--device cuda:0 \
 	--new_class_embedding --smart_mapping \
 	--output_dir logs/HWDB -c config/HWDB_full.py --dataset_file HWDB --save_results \
