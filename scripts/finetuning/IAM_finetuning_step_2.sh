3cfae97bbe3dcb9376b259ea06cfb7c720eb3bde
Raphael Baena <38126139+raphael-baena@users.noreply.github.com>
2024-11-04 19:50:28 +0100

    Update IAM_finetuning_step_2.sh

--- scripts/finetuning/IAM_finetuning_step_2.sh
+++ scripts/finetuning/IAM_finetuning_step_2.sh
@@ -3,7 +3,7 @@ python finetuning.py \
 	--new_class_embedding --smart_mapping \
 	--random_erasing \
 	--resume_finetuning \
-	--output_dir logs/OCR_IAM -c config/Latin_CTC.py --dataset_file IAM --save_results \
+	--output_dir logs/IAM -c config/Latin_CTC.py --dataset_file IAM --save_results \
 	--options dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
 	dn_box_noise_scale=1.0
