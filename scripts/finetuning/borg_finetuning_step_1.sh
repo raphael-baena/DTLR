ccb3dccab58aea7a02fa619e39565b44746edfc9
Raphael Baena <38126139+raphael-baena@users.noreply.github.com>
2024-11-04 19:50:37 +0100

    Update borg_finetuning_step_1.sh

--- scripts/finetuning/borg_finetuning_step_1.sh
+++ scripts/finetuning/borg_finetuning_step_1.sh
@@ -1,7 +1,7 @@
 python finetuning.py \
 	--device cuda:0 \
 	--new_class_embedding \
-	--output_dir logs/OCR_borg -c config/Latin_CTC.py --dataset_file borg --save_results \
+	--output_dir logs/borg -c config/Latin_CTC.py --dataset_file borg --save_results \
 	--options dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
 	dn_box_noise_scale=1.0
