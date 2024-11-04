27d05b3d19ba033561ba9ae06f544e2c70f29a6a
Raphael Baena <38126139+raphael-baena@users.noreply.github.com>
2024-11-04 19:51:14 +0100

    Update Chinese_step_2.sh

--- scripts/finetuning/Chinese_step_2.sh
+++ scripts/finetuning/Chinese_step_2.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=0 python main_RIMES.py \
 	--new_class_embedding --smart_mapping \
 	--resume_finetuning \
 	--path_old_charset /home/rbaena/projects/OCR/DINO/charset_HWDB.pkl \
-	--output_dir logs/DINO/test_HWDB -c config/DINO/HWDB_full.py --dataset_file HWDB --save_results \
+	--output_dir logs/HWDB -c config/DINO/HWDB_full.py --dataset_file HWDB --save_results \
 	--options dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
 	dn_box_noise_scale=1.0
