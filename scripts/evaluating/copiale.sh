fabec0c19635c9a262abb3068caa00870a9bef05
Raphael Baena <38126139+raphael-baena@users.noreply.github.com>
2024-11-06 16:37:44 +0100

    Update copiale.sh

--- scripts/evaluating/copiale.sh
+++ scripts/evaluating/copiale.sh
@@ -1,2 +1,2 @@
-weight_path="/logs/Copiale"
+weight_path="./logs/Copiale/checkpoint.pth"
 python evaluation.py --dataset copiale --mode test --new_class_embedding  --metrics cipher --weights $weight_path --config config/Latin_CTC.py 
