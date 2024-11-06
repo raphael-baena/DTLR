349efccd0d15a738e76e11eabc7834f7a67c2541
Raphael Baena <38126139+raphael-baena@users.noreply.github.com>
2024-11-06 16:36:21 +0100

    Update HWDB.sh

--- scripts/evaluating/HWDB.sh
+++ scripts/evaluating/HWDB.sh
@@ -1,2 +1,2 @@
-weight_path="/logs/HWDB"
+weight_path="./logs/HWDB/checkpoint.pth"
 python evaluation.py --dataset HWDB --mode test --new_class_embedding --new_label_enc --metrics chinese --weights $weight_path --config config/HWDB_full.py --unicode
