0c929578c5a8ee009622bdfd42b56de08c630b89
Raphael Baena <38126139+raphael-baena@users.noreply.github.com>
2024-11-06 16:36:44 +0100

    Update IAM.sh

--- scripts/evaluating/IAM.sh
+++ scripts/evaluating/IAM.sh
@@ -1,3 +1,3 @@
-weight_path="/logs/IAM"
+weight_path="./logs/IAM/checkpoint.pth"
 python evaluation.py --dataset IAM --mode test  --weights $weight_path --config config/Latin_CTC.py \
 --NMS 0.5 --TH 0.3  
