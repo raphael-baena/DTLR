88c1de3cb0fdf371ec582e0e6d902a97c2e46671
Raphael Baena <38126139+raphael-baena@users.noreply.github.com>
2024-11-06 16:37:27 +0100

    Update borg.sh

--- scripts/evaluating/borg.sh
+++ scripts/evaluating/borg.sh
@@ -1,2 +1,2 @@
-weight_path="/logs/Borg"
+weight_path="./logs/Borg/checkpoint.pth"
 python evaluation.py --dataset borg --mode test --new_class_embedding  --metrics cipher --weights $weight_path --config config/Latin_CTC.py 
